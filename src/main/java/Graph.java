17148dd3e89e518430f859e5eb493c4b7de88210
Erik Gustafsson <erikgust2@gmail.com>
2022-02-25 14:49:24 +0100

    Påbörjad Graph.java

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Graph {
+
+    private final HashMap<String, ArrayList<Actor>> graphMap;
+    private final HashSet<Actor> actors;
+
+    /**
+     * Konstruktor
+     *
+     * @param file Textfilen som innehåller skådespelare och filmer
+     */
+    public Graph(String file) {
+        graphMap = new HashMap<>();
+        actors = new HashSet<>();
+        buildGraph(file);
+    }
+
+    private void buildGraph(String file) {
+
+    }
+
+    private void addActorsFromMovie(String movieName) {
+
+    }
+
 }
