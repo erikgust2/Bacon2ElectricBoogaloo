de00aba8e0fbe19e41cbf92071e1ef3a1969466b
anton <anlu6569@win.dsv.su.se>
2022-02-25 14:58:41 +0100

    Inläsning

--- src/main/java/MainProgram.java
+++ src/main/java/MainProgram.java
@@ -1,24 +1,51 @@
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class MainProgram {
-    /*private void makeGraph(String fileName) throws IOException {
-        BufferedReader in = new BufferedReader(new FileReader(fileName));
-        String nextLine;
-        String movie;
-        nextLine = in.readLine();
-        //Parse the file
-        while(nextLine != null) {
-            String[] parsed = nextLine.split("/");
-            movie = parsed[0];
-            for(int i=1; i<parsed.length; i++) {
-               );
-            }
 
-            nextLine = in.readLine();
+    public void makeGraph(String fileName) /*throws FileNotFoundException*/ {
+      /*  try {
+            FileReader reader = new FileReader(fileName);
+            BufferedReader in = new BufferedReader(reader);
+            String line = null;
+            line = in.readLine();
+            while line 1= null{
+
+                String[] tokens = line.split(";");
+                String command = tokens[0];
+                if(command.equals("a")){
+                    String actor = tokens[1];
+                }
+                if(command.equals("t")){
+                    String movie = tokens[1];
+                }
+
+                line = in.readLine();
+
+                int genresInt = Integer.parseInt(line);
+                Set<String> genres = new HashSet<>();
+                for (int j = 0; j < genresInt; j++) {
+                    //loop för genre
+                    String genre = in.readLine();
+                    genres.add(genre);
+                }
+                Recording r = new Recording(title, artist, year, genres);
+                recordings.add(new Recording(title, artist, year, genres));
+                line = in.readLine();
+            }
+            reader.close();
+            in.close();
+
+        } catch (FileNotFoundException e) {
+            throw new FileNotFoundException("File not found!");
+        } catch (IOException e) {
+            System.err.println("IO-fel! " + e.getMessage());
         }
-        in.close();
+
     }*/
 
+
+    }
 }
